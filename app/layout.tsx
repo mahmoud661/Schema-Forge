52d9635ef4148824b645a738883326e889cf5558
mahmoud zuriqi <mahmoudzuriqi8@gmail.com>
2025-03-14 14:05:00 +0300

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Metadata } from "next";
 import { Providers } from "./providers";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
@@ -18,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Analytics/>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
